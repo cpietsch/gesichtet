94d7cce40107e27ef9a7f771eb267257ce386c8b
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-07-03 23:32:48 +0200

    update names.js and view

--- node-server/src/names.js
+++ node-server/src/names.js
@@ -8,11 +8,11 @@ var data = require('../../data/names.json');
 
 exports.getData = function getData() {
   // die totale menge im array
-  var totalMessages = data.zitate.length;
+  var totalMessages = data.names.length;
 
   // ein random wert für die abfrage aus dem array
   var random = utils.getRandom(0, totalMessages-1);
   //console.log(random);
 
-  return data.messages[random];
+  return data.names[random];
 };
